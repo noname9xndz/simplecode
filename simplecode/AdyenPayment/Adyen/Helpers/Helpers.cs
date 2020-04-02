fc579585200b35299b6163306dc7a1480a62c31d
noname9xnd <noname9xnd@gmail.com>
2020-04-02 15:46:35 +0700

    update request adyen

---
+++ simplecode/AdyenPayment/Adyen/Helpers/Helpers.cs
@@ -0,0 +1,52 @@
+﻿using AdyenPayment.Adyen.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdyenPayment.Adyen.Helpers
+{
+    public static class Helpers
+    {
+        public static decimal ConvertCurrentcyAdyen(string money, string currency)
+        {
+            try
+            {
+                CurrencyResponse response = new CurrencyResponse();
+                string stateDataPath = AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.IndexOf("/Adyen/Files/currency.json", StringComparison.Ordinal));
+                if (System.IO.File.Exists(stateDataPath))
+                {
+                    var stateInfo = System.IO.File.ReadAllText(stateDataPath);
+                    var items = JsonConvert.DeserializeObject<List<CurrencyResponse>>(stateInfo);
+                    if (items != null && items != null)
+                    {
+                        if (currency != null)
+                        {
+                            response = items.Where(x => x.Code == currency).FirstOrDefault();
+                        }
+                        else
+                        {
+                            response = items.FirstOrDefault();
+                        }
+                    }
+                    if (response.Point == "2")
+                    {
+                        return decimal.Parse(money) * 100;
+                    }
+                    if (response.Point == "3")
+                    {
+                        return decimal.Parse(money) * 1000;
+                    }
+                    if (response.Point == "0")
+                    {
+                        return decimal.Parse(money);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return decimal.Parse(money);
+        }
+    }
+}
---
+++ simplecode/AdyenPayment/Adyen/Models/Response/PaymentDetailsResponse.cs
@@ -0,0 +1,116 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace AdyenPayment.Adyen.Models
+{
+    public class PaymentDetailsResponse
+    {
+        [JsonProperty("groups")]
+        public List<groups> Groups { get; set; }
+
+        [JsonProperty("oneClickPaymentMethods")]
+        public List<oneClickPaymentMethods> OneClickPaymentMethods { get; set; }
+
+        [JsonProperty("paymentMethods")]
+        public List<paymentMethods> PaymentMethods { get; set; }
+
+        [JsonProperty("storedPaymentMethods")]
+        public List<StoredPaymentMethods> StoredPaymentMethods { get; set; }
+    }
+
+    public class groups
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("types")]
+        public List<string> Types { get; set; }
+    }
+
+    public class oneClickPaymentMethods
+    {
+        [JsonProperty("details")]
+        public List<details> Details { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("recurringDetailReference")]
+        public string RecurringDetailReference { get; set; }
+
+        [JsonProperty("storedDetails")]
+        public storedDetails StoredDetails { get; set; }
+    }
+
+    public class paymentMethods
+    {
+        [JsonProperty("brands")]
+        public List<string> Brands { get; set; }
+
+        [JsonProperty("details")]
+        public List<details> Details { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public class storedDetails
+    {
+        [JsonProperty("card")]
+        public card Card { get; set; }
+    }
+
+    public class card
+    {
+        [JsonProperty("expiryMonth")]
+        public string ExpiryMonth { get; set; }
+
+        [JsonProperty("expiryYear")]
+        public string ExpiryYear { get; set; }
+
+        [JsonProperty("holderName")]
+        public string HolderName { get; set; }
+
+        [JsonProperty("number")]
+        public string Number { get; set; }
+    }
+
+    public class details
+    {
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+
+    public class StoredPaymentMethods
+    {
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+
+        [JsonProperty("holderName")]
+        public string HolderName { get; set; }
+
+        [JsonProperty("lastFour")]
+        public string LastFour { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("supportedShopperInteractions")]
+        public List<string> SupportedShopperInteractions { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
