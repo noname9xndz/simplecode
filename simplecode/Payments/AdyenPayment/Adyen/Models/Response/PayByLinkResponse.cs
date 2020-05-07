fc7d4d0ba6b36e3fd4bef8befd88190494717c4e
Cuong <cuongnv@smartosc.com>
2020-05-07 14:36:04 +0700

    Merge branch 'master' into dev-AdyenPayment

---
+++ simplecode/Payments/AdyenPayment/Adyen/Models/Response/PayByLinkResponse.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace AdyenPayment.Adyen.Models
+{
+    public class PayByLinkResponse
+    {
+        public PayByLinkSuccess PayByLink { get; set; }
+        public PayByLinkError PayByLinkError { get; set; }
+
+        public PayByLinkResponse()
+        {
+            PayByLink = new PayByLinkSuccess();
+            PayByLinkError = new PayByLinkError();
+        }
+    }
+
+    public class PayByLinkError
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("errorCode")]
+        public string ErrorCode { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("errorType")]
+        public string ErrorType { get; set; }
+    }
+
+    public class PayByLinkSuccess
+    {
+        [JsonProperty("amount")]
+        public Amount Amount { get; set; }
+
+        [JsonProperty("expiresAt")]
+        public DateTime ExpiresAt { get; set; }
+
+        [JsonProperty("reference")]
+        public string Reference { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
